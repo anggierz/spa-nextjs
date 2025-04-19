c1a998732f8a37deb3493226243468bf7412bb7a
anggierz <anggie.rizo.zambrano@gmail.com>
2025-04-19 15:44:19 +0200

    cambio ruta search a use client para evitar recarga de la pagina (NOOK)

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,81 +1,74 @@
+'use client';
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Header from "@/components/Header";
 
-async function fetchMovieByTitle(title) {
-  if (!title) return [];
+export default function Search() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(true);
+    const [query, setQuery] = useState("");
 
-  const token = process.env.TMDB_TOKEN;
+    const token = process.env.TMDB_TOKEN;
 
-  try {
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${token}`
+    async function fetchMovieByTitle(title) {
+        try {
+            const options = {
+                method: 'GET',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: `Bearer ${token}`
+                }
+            };
+        
+            const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`, options);
+            const data = await response.json();
+            setMovies(data.results);
+
+        } catch (error) {
+            console.error("Error fetching movie: ", error);
         }
-      };
-    
-      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`, options);
-
-      const data = await response.json();
-      return data.results;
+        finally {
+            setLoading(false);
+        }
     }
 
-    catch (err) {
-      console.error("Error fetching movie: ", err);
-      return [];
+    const handleSearch = () => {
+        console.log(query);
+        setLoading(true);
+        setError(null);
+
+        if (!query)
+            setError("Please, enter a valid search term.");
+
+        fetchMovieByTitle(query);
     }
 
-  }
-
-
-export default async function SearchPage({ searchParams }) {
-  const query = searchParams.q || "";
-  const movies = await fetchMovieByTitle(query);
-  const error = query && !movies.length;
-
-  return (
-    <div>
-      <Header />
-      <h1>Search</h1>
-      <p>Search for a movie.</p>
-      <form className="search-container" method="GET">
-        <input type="search" name="q" className="search-input" defaultValue={query} />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-      </form>
-
-      {query && (
-        <>
-          {error ? (
-            <p style={{ color: "red" }}>No movies found.</p>) : (
-            <div className="container">
-              {movies.map((movie) => (
-                <Link href={`/movie/${movie.id}`} key={movie.id} className="nav-link">
-                  <div className="movie" key={movie.id}>
-                    <Image
-                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                      alt={movie.title}
-                      width={247}
-                      height={370}
-                    />
-                    <h2>{movie.title}</h2>
-                    <p>
-                        {
-                            movie.overview.length >= 150 ?
-                            `${movie.overview.substring(0, 150)}[...]` : movie.overview
-                        }
-                    </p>
-                    <p>Release: {movie.release_date}</p>
-                  </div>
-                </Link>
-              ))}
+    return (
+        <div>
+            <Header />
+            <h1>Search</h1>
+            <p>Search for a movie.</p>
+            <div className="search-container">
+                <input type="search" id="site-search" name="q" className="search-input" onChange={(e) => setQuery(e.target.value)} />
+                <button onClick={handleSearch} className="search-button" disabled={loading}>Search</button>
             </div>
-          )}
-        </>
-      )}
-    </div>
-  );
+            {loading && <p>Loading...</p>}
+            {movies.length == 0 && !loading && !error && <p>No movies found.</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <div className="container">
+            {movies.map(movie => (
+                <Link href={`/movie/${movie.id}`} key={movie.id} className="nav-link">
+                    <div className="movie" key={movie.id}>
+                        <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} width={247} height={370}/>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.overview}</p>
+                        <p>Release: {movie.release_date}</p>
+                    </div>
+                </Link>
+            ))}
+            </div>
+        </div>
+    );
 }
